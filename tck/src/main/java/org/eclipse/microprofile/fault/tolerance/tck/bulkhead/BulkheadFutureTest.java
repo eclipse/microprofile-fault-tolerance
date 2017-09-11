88b1b36fae0e3d211ac82b16e6cb0ee5e7c87920
Gordon Hutchison <Gordon.Hutchison@gmail.com>
2017-09-11 16:37:42 +0100

    tck Future.cancel test in error
    Signed-off-by: Gordon Hutchison <Gordon.Hutchison@gmail.com>

--- tck/src/main/java/org/eclipse/microprofile/fault/tolerance/tck/bulkhead/BulkheadFutureTest.java
+++ tck/src/main/java/org/eclipse/microprofile/fault/tolerance/tck/bulkhead/BulkheadFutureTest.java
@@ -19,7 +19,6 @@
  *******************************************************************************/
 package org.eclipse.microprofile.fault.tolerance.tck.bulkhead;
 
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
@@ -68,46 +67,6 @@ public class BulkheadFutureTest extends Arquillian {
     @BeforeTest
     public void beforeTest(final ITestContext testContext) {
         Utils.log("Testmethod: " + testContext.getName());
-    }
-
-    /**
-     * Tests that the Future that is returned from a asynchronous bulkhead
-     * method can be cancelled OK and that isCancelled works correctly.
-     */
-    @Test()
-    public void testBulkheadMethodAsynchFutureCancel() {
-
-        // We want a long running backend that we can cancel
-        Checker fc = new FutureChecker(LONG_TIME);
-
-        Future<String> result = null;
-        try {
-            result = bhBeanMethodAsynchronousDefault.test(fc);
-        }
-        catch (InterruptedException e1) {
-            Assert.fail("Unexpected interruption", e1);
-        }
-
-        Assert.assertFalse(result.isDone(), "Future reporting Done when not");
-        Assert.assertFalse(result.isCancelled(), "Future reporting Canceled when not");
-
-        result.cancel(true);
-
-        Utils.sleep(SHORT_TIME);
-
-        Assert.assertTrue(result.isDone(), "Future reporting Done when not");
-        Assert.assertTrue(result.isCancelled(), "Future reporting not Cancelled when Cancelled");
-
-        try {
-            String rc = result.get();
-            Assert.assertNull(rc, "We should have gotten a CancelationException as cancelled");
-        }
-        catch (Throwable t) {
-            Assert.assertTrue(t instanceof CancellationException);
-        }
-        Assert.assertTrue(result.isCancelled(), "Future cancel not reporting Cancelled after get");
-        Assert.assertTrue(result.isDone(), "Future not reporting Done when canceled after get");
-
     }
 
     /**
@@ -165,50 +124,7 @@ public class BulkheadFutureTest extends Arquillian {
         }
         Assert.assertTrue(result.isDone(), "Future done not reporting true");
     }
-
-    /**
-     * Tests that the Future that is returned from a asynchronous bulkhead can
-     * be canceled OK and that isCancelled works correctly on a method in an
-     * asynchronous bulkhead annotated class
-     */
-    @Test()
-    public void testBulkheadClassAsynchFutureCancel() {
-
-        Checker fc = new FutureChecker(LONG_TIME);
-        Future<String> result = null;
-        try {
-            result = bhBeanClassAsynchronousDefault.test(fc);
-        }
-        catch (InterruptedException e1) {
-            Assert.fail("Unexpected interruption", e1);
-        }
-
-        Assert.assertFalse(result.isDone(), "Future reporting Done when not");
-        Assert.assertFalse(result.isCancelled(), "Future reporting Canceled when not");
-
-        result.cancel(true);
-
-        try {
-            Thread.sleep(SHORT_TIME);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Assert.assertTrue(result.isDone(), "Future reporting not Done when Done");
-        Assert.assertTrue(result.isCancelled(), "Future reporting not Cancelled when Cancelled");
-
-        try {
-            String rc = result.get();
-            Assert.assertNull(rc, "We should have got a CancelationException");
-        }
-        catch (Throwable t) {
-            Assert.assertTrue(t instanceof CancellationException);
-        }
-        Assert.assertTrue(result.isCancelled(), "Future isCancelled not reporting true when called after cancel,get");
-        Assert.assertTrue(result.isDone(), "Future not reporting Done when called after cancel,get");
-
-    }
-
+  
     /**
      * Tests that the Future that is returned from a asynchronous bulkhead can
      * be queried for Done OK after a goodpath get with timeout and also
