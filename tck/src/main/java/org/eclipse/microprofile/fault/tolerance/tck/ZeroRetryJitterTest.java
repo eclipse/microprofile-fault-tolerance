71aca6fa646729727a0dcf6004344d44b750151f
Doychin Bondzhev <doychin@dsoft-bg.com>
2019-01-08 14:55:52 +0200

    Add test to validate proper execution when jitter = 0
    Incorrectly written code can lead to error during execution if jitter = 0 is not properly processed.
    
    Signed-off-by: Doychin Bondzhev <doychin@dsoft-bg.com>

---
+++ tck/src/main/java/org/eclipse/microprofile/fault/tolerance/tck/ZeroRetryJitterTest.java
@@ -0,0 +1,69 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2017 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.eclipse.microprofile.fault.tolerance.tck;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.fault.tolerance.tck.retry.clientserver.RetryClientForZeroJitter;
+import org.eclipse.microprofile.faulttolerance.exceptions.FaultToleranceDefinitionException;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.ShouldThrowException;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class ZeroRetryJitterTest extends Arquillian {
+
+    @Inject
+    private RetryClientForZeroJitter zeroJitterClient;
+
+    @Deployment
+    @ShouldThrowException(value = FaultToleranceDefinitionException.class)
+    public static WebArchive deploy() {
+        JavaArchive testJar = ShrinkWrap
+            .create(JavaArchive.class, "ftZeroTestJitter.jar")
+            .addClasses(RetryClientForZeroJitter.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+            .as(JavaArchive.class);
+
+        return ShrinkWrap
+            .create(WebArchive.class, "ftZeroTestJitter.war")
+            .addAsLibrary(testJar);
+    }
+    /**
+     * Test that checks that jitter = 0 does not generate error during method call.
+     * 
+     * A Service is annotated with a @Retry annotation with jitter = 0.
+     */
+    @Test
+    public void test() {
+        try {
+            zeroJitterClient.serviceA();
+
+            Assert.fail("This should not happen");
+        } catch (RuntimeException e) {
+            // expected
+        }
+    }
+}
