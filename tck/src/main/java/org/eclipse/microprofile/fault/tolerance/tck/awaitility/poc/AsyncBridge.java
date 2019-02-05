ec11addbfa22565a0e35165749838313e84befa7
Carlos Andres De La Rosa <kusanagi12002@gmail.com>
2019-02-06 00:44:40 +0100

    Awaitility Proof Of concept
    https://github.com/eclipse/microprofile-fault-tolerance/issues/73
    
    No merge
    
    Signed-off-by: Carlos Andres De La Rosa <kusanagi12002@gmail.com>

---
+++ tck/src/main/java/org/eclipse/microprofile/fault/tolerance/tck/awaitility/poc/AsyncBridge.java
@@ -0,0 +1,44 @@
+/*
+ *******************************************************************************
+ * Copyright (c) 2019 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.eclipse.microprofile.fault.tolerance.tck.awaitility.poc;
+
+public class AsyncBridge {
+
+    private final Task task;
+
+    public AsyncBridge(Task task) {
+        this.task = task;
+    }
+
+    public void perform() {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(600);
+                    task.doTask("Task Done!!");
+                }
+                catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+    }
+}
